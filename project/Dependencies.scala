0452e62b8d9c1a870277bbae1a45bbb12f224bc6
xuwei-k <6b656e6a69@gmail.com>
2026-07-03 16:27:08 +0900

    Update dependencies. Drop Scala 2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,9 +1,6 @@
 object Dependencies {
-  lazy val scala210 = "2.10.7"
-  lazy val scala211 = "2.11.12"
-  lazy val scala212 = "2.12.19"
-  // last of scala.js 1.12.0
-  lazy val scala213 = "2.13.13"
-  lazy val scala3 = "3.3.6"
+  lazy val scala212 = "2.12.21"
+  lazy val scala213 = "2.13.18"
+  lazy val scala3 = "3.3.8"
   val verifyVersion = "1.0.0"
 }
